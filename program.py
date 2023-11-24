bc6cb6523dd54e18caac05092609f36dc4aeb331
Miloslav Cerny <cernym@jablotron.cz>
2023-11-24 15:41:56 +0100

    serial communication init

--- program.py
+++ program.py
@@ -6,6 +6,16 @@ Created on Fri Nov 24 15:04:37 2023
 """
 
 import time
+import serial
+
+ser = serial.Serial(
+    port='COM5',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+        timeout=0)
+
 print("Program started")
 
 while True:
